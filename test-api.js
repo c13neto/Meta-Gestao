25fd99171868873a0c17c1a5f91068cab4fbb82a
c13neto <adalbertoneto13@protonmail.com>
2026-09-24 11:06:53 -0300

    tirar todos os teste e codigo não necessário

--- test-api.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-// We can't easily run Angular services without full setup, 
-// let's just inspect the logic to see if we missed something.
-console.log("No obvious error found.");
--- test-login.js
+++
@@ -1,4 +0,0 @@
-const query = `action=login&email=${encodeURIComponent('admin@admin.com')}&senha=${encodeURIComponent('123')}`;
-const params = new URLSearchParams(query);
-console.log(params.get('email'));
-console.log(params.get('senha'));
